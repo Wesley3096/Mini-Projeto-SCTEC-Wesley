af54374b1b622cae00baf1ca0abeee0b1a733ac6
Wesley3096 <wesley96vieira@gmail.com>
2026-08-03 21:11:38 -0300

    feat: adds structure for vacancies and candidates

--- skillmatch.js
+++ skillmatch.js
@@ -1,1 +1,83 @@
-g
+const jobs = [
+   {id = job01,
+    title = "Desenvolvedor Front-End Jr",
+    company = "FrontCode",
+    requirements = ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Trello",],
+    experience = "1-2 years",
+    },
+        {id = job02,
+        title = "Desenvolvedor Front-End Jr",
+        company = "SkySoft",
+        requirements = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "TypeScript"],
+    experience = "3-4 years",
+    },
+
+        {id = job03,
+         title = "Desenvolvedor Front-End Jr",
+         company = "Infinity Software",
+         requirements = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "Jira"],
+         experience = "1 year",
+         },
+
+        { id = job04,
+          title = "Desenvolvedor Front-End Jr",
+          company = "BlueLogic",
+          requirements = ["HTML", "CSS", "JavaScript", "React", "Vite", "Git", "GitHub"],
+          experience = "2 years",
+        },
+
+
+];
+
+const candidates = [
+    {id = candidate01,
+     name = "João Silva",
+     skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub"],
+     experience = "2 years",
+    },
+
+    {id = candidate02,
+     name = "Maria Souza",
+     skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "TypeScript"],
+     experience = "3 years",
+    },
+
+    {id = candidate03, 
+    name = "Lucas Santos",
+    skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub"],
+    experience = "5 year",
+    },
+
+    {id = candidate04,
+     name = "Bruno Fernandes",
+     skills = ["HTML", "CSS", "JavaScript", "React", "Vite", "Git", "GitHub"],
+     experience = "2 years",
+    },
+
+    {id = candidate05,
+     name = "Ana Oliveira",
+     skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "TypeScript"],
+     experience = "6 year",
+    },
+
+    {id = candidate06,
+     name = "Carla Lima",
+     skills = ["HTML", "CSS", "JavaScript", "React", "Git", "GitHub", "Trello"],
+     experience = "6 months",
+    },
+
+    {id = candidate07,
+     name = "Pedro Costa",
+     skills = ["HTML", "CSS","Git", "GitHub"],
+     experience = "10 months",
+
+    },
+
+    {id = candidate08,
+     name = "Fernanda Rocha",
+     skills = ["HTML", "CSS", "JavaScript","Jira"],
+     experience = "5 months",
+    },
+
+]
+
